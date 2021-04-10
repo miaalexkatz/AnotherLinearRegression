4371950c447a999bfa926fb553a5d57feda6de40
Mia Katz <37454023+miaalexkatz@users.noreply.github.com>
2021-04-10 18:07:16 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,42 @@
+var data = [];
+
+var m = 0;
+var b = 0;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function gradientDescent() {
+  var slider = document.getElementById("myRange");
+  var learning_rate = slider.value/1000;
+  var output = document.getElementById("learnrate");
+  output.innerHTML = slider.value/1000;
+  for (var i = 0; i < data.length; i++) {
+    var x = data[i].x;
+    var y = data[i].y;
+    var guess = m * x + b;
+    var error = y - guess;
+    m = m + error * x * learning_rate;
+    b = b + error * learning_rate;
+  }
+}
+
+function drawLine() {
+  var x1 = 0;
+  var y1 = m * x1 + b;
+  var x2 = 1;
+  var y2 = m * x2 + b;
+
+  x1 = map(x1, 0, 1, 0, width);
+  y1 = map(y1, 0, 1, height, 0);
+  x2 = map(x2, 0, 1, 0, width);
+  y2 = map(y2, 0, 1, height, 0);
+
+  stroke(255, 188, 58);
+  strokeWeight(2);
+  line(x1, y1, x2, y2);
+  document.getElementById("result").innerHTML = m;
 }
 
 function mousePressed() {
